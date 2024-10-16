0bf6faef4917a174b047e70945d9ce2d0e062f0f
Nasser Ali <nasserali60@live.com>
2024-10-15 22:54:44 -0230

    Added unit tests for question generation and answer checking

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,5 +1,16 @@
 const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 
-describe("Tests for getQuestion", () => {});
+describe("Tests for getQuestion", () => {
+  test("Checks if question follows the [number] [operator] [number] format", () => {
+    expect(getQuestion()).toMatch(/\d\s[+\-*/]\s\d/);
+  });
+});
 
-describe("Tests for isCorrectAnswer", () => {});
+describe("Tests for isCorrectAnswer", () => {
+  test("Checks if the answer is correct", () => {
+    expect(isCorrectAnswer("2 + 2", "4")).toBe(true);
+  });
+  test("Checks if the answer is incorrect", () => {
+    expect(isCorrectAnswer("2 + 2", "10")).toBe(false);
+  });
+});
